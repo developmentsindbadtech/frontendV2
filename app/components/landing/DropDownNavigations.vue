35847e3fec637e140533a72ba7b65a2b1338eb32
jeremy-sinbad <jeremy.estrada@sindbad.tech>
2026-03-02 14:09:03 +0800

    feat: enhance dropdown button and content layout for improved styling and responsiveness

--- app/components/landing/DropDownNavigations.vue
+++ app/components/landing/DropDownNavigations.vue
@@ -10,17 +10,26 @@ const props = defineProps<{
 <template>
   <DropdownMenu>
     <DropdownMenuTrigger as-child>
-      <Button variant="default" size="sm" class="hover:bg-background hover:text-secondary">
+      <Button
+        variant="default"
+        size="sm"
+        class="hover:bg-transparent hover:text-secondary bg-transparent border-0 text-primary focus:ring-0 focus:ring-offset-0 data-[state=open]:bg-transparent data-[state=open]:text-secondary"
+      >
         <span class="text-lg">{{ props.title }}</span>
         <ChevronDown class="ml-2 h-4 w-4" />
       </Button>
     </DropdownMenuTrigger>
-    <DropdownMenuContent class="w-56" align="start">
-      <DropdownMenuItem>
-        <span>{{ props.title }}</span>
+
+    <DropdownMenuContent
+      align="start"
+      class="fixed left-0 right-0 w-screen max-w-none rounded-none p-1"
+    >
+      <DropdownMenuItem class="w-full justify-start">
+        <span class="block w-full">{{ props.title }}</span>
       </DropdownMenuItem>
-      <DropdownMenuItem v-for="item in props.menuItems" :key="item">
-        <span>{{ item }}</span>
+
+      <DropdownMenuItem v-for="item in props.menuItems" :key="item" class="w-full justify-start">
+        <span class="block w-full">{{ item }}</span>
       </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
