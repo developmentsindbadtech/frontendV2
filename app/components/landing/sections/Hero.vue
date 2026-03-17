3987ba27adc8281d9d1feb7342b097cb387383b5
jeremy-sinbad <jeremy.estrada@sindbad.tech>
2026-03-17 14:45:05 +0800

    feat: add text-gradient utility and enhance layout for Hero and Feature sections

--- app/components/landing/sections/Hero.vue
+++ app/components/landing/sections/Hero.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
-import { Sparkles } from 'lucide-vue-next'
-
 import { ref, onMounted } from 'vue'
 const divRef = ref<HTMLElement | null>(null)
+
+const trustedBrands = [
+  { name: 'Alkhabeer Capital', logo: '/images/trusted-brands/alkhabeer-capital.png' },
+  { name: 'Riyad Capital', logo: '/images/trusted-brands/riyad-capital.png' },
+  { name: 'Microsoft', logo: '/images/trusted-brands/microsoft.png' },
+  { name: 'Startupbootcamp', logo: '/images/trusted-brands/startup-bootcamp.png' },
+  { name: 'Flat6LABS', logo: '/images/trusted-brands/flat-6labs.png' },
+  {
+    name: 'Center of Digital Entrepreneurship',
+    logo: '/images/trusted-brands/center-of-digital.png',
+  },
+  {
+    name: 'Misk',
+    logo: '/images/trusted-brands/misk.png',
+  },
+]
 
 onMounted(() => {
   const observer = new IntersectionObserver(
@@ -24,30 +38,25 @@ onMounted(() => {
 </script>
 
 <template>
-  <section class="relative h-screen z-1 w-full mx-auto px-4 sm:px-6 py-3 pb-12 md:pb-16 lg:px-0">
-    <div class="absolute inset-0 -z-10">
-      <NuxtImg
-        src="/images/hero_bg.png"
-        class="h-full w-full object-cover object-top"
-        alt="Hero Background"
-      />
+  <section class="relative z-1 w-full mx-auto px-4 sm:px-6 py-3 pb-12 md:pb-16 lg:px-0">
+    <div class="absolute inset-0 -z-10 -bottom-190">
+      <NuxtImg src="/images/hero_bg.png" class="h-full w-full object-cover" alt="Hero Background" />
     </div>
     <div class="w-full min-h-155 flex flex-col items-center justify-center pt-6 md:pt-10">
-      <span
-        class="flex items-center gap-2 mt-16 animate-fade-in animation-delay-100 px-4 py-2 rounded-full border border-secondary font-bold text-base sm:text-sm text-white bg-secondary/45 w-max mx-auto mb-6 shadow-secondary/40 shadow-lg"
-      >
-        <Sparkles class="h-4 w-4" />
-        Trading decisions are powered by AI
-      </span>
+      <div class="flex flex-wrap items-center gap-6 mx-auto text-white">
+        <NuxtImg src="/images/cma.png" alt="CMA Logo" />
+        <span>|</span>
+        <p class="text-base">Permit No.:2023-155-05</p>
+      </div>
 
       <!-- Text-Content -->
       <div
         class="w-full max-w-sm md:max-w-3xl lg:max-w-5xl md:mt-6 animate-fade-in animation-delay-200"
       >
         <h1
-          class="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl tracking-tight text-center text-secondary"
+          class="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl tracking-tight text-center text-white leading-snug"
         >
-          Build Your Legacy <span class="text-primary">While You Live It.</span>
+          Build Your Legacy <span class="text-gradient!">While You Live It.</span>
         </h1>
 
         <p
@@ -75,15 +84,12 @@ onMounted(() => {
         </h1>
 
         <!-- Trusted brands logos placeholder -->
-        <div class="flex flex-col md:flex-row gap-6 md:gap-0 justify-between items-center mt-6">
+        <div class="flex flex-col md:flex-row gap-10 md:gap-6 justify-between items-center mt-6">
           <NuxtImg
-            v-for="value in 6"
-            :key="value"
-            src="/images/brand-placeholder.png"
-            alt="Trusted brand logo"
-            width="120"
-            height="48"
-            class="w-30 h-auto mx-auto"
+            v-for="brand in trustedBrands"
+            :key="brand.name"
+            :src="brand.logo"
+            :alt="`${brand.name} Logo`"
           />
         </div>
       </div>
